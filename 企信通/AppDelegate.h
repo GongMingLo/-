2a5d121c1541f4192bb323513cdc9a303c84db5b
apple <apple@teacher.local>
2013-11-30 09:21:33 +0800

    Initial Commit

---
+++ 企信通/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  企信通
+//
+//  Created by apple on 13-11-30.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 企信通/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  企信通
+//
+//  Created by apple on 13-11-30.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
